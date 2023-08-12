8cbe268d592842a526318e252e856ebd12dd37e4
M-Farhaan-Iqbal <44829074+M-Farhaan-Iqbal@users.noreply.github.com>
2023-08-12 18:54:40 +0400

    remove content as necessary

--- App/lambda_function.py
+++ App/lambda_function.py
@@ -22,7 +22,7 @@ db = SQLAlchemy(app)
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True, nullable=False)
-    content = db.Column(db.String(120), unique=True, nullable=False)
+    content = db.Column(db.String(120), nullable=False)
 
     def __init__(self, title, content):
         self.title = title
