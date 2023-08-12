474b75a1deaf8c21c8417d4a84a1e937728d6ac9
M-Farhaan-Iqbal <44829074+M-Farhaan-Iqbal@users.noreply.github.com>
2023-08-12 16:35:40 +0400

    cleaned up code

--- App/lambda_function.py
+++ App/lambda_function.py
@@ -4,93 +4,104 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import logging
 
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-import sys
-
 app = Flask(__name__)
 
+# Load environment variables
+db_config = {
+    'user': os.environ.get('PGUSER'),
+    'password': os.environ.get('PGPASSWORD'),
+    'host': os.environ.get('PGHOST'),
+    'port': os.environ.get('PGPORT'),
+    'database': os.environ.get('PGDATABASE'),
+}
 
-
-# rebuilding from environment variables
-dburl = "postgresql://"+ os.environ.get('PGUSER') + ":" + os.environ.get('PGPASSWORD') + "@" + os.environ.get('PGHOST') +  ":" + os.environ.get('PGPORT') + "/" +  os.environ.get('PGDATABASE')
-
-logger.info('## Connection to db ')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = dburl
+db_url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**db_config)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 db = SQLAlchemy(app)
 
 class Item(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(80), unique=True, nullable=False)
-  content = db.Column(db.String(120), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    content = db.Column(db.String(120), unique=True, nullable=False)
 
-  def __init__(self, title, content):
-    self.title = title
-    self.content = content
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
 
-with app.app_context():
-    db.create_all()
+db.create_all()
 
-@app.route('/items/<id>', methods=['GET'])
+# Helper function to convert Item object to JSON dict
+def item_to_dict(item):
+    return {
+        'id': item.id,
+        'title': item.title,
+        'content': item.content,
+    }
+
+# CRUD Endpoints
+@app.route('/items/<int:id>', methods=['GET'])
 def get_item(id):
     item = Item.query.get(id)
     if item is None:
         return jsonify(message='Item not found'), 404
-    
-    del item.__dict__['_sa_instance_state']
-    return jsonify(item.__dict__)
+    return jsonify(item_to_dict(item))
 
 @app.route('/items', methods=['GET'])
 def get_items():
-  items = []
-  for item in db.session.query(Item).all():
-    del item.__dict__['_sa_instance_state']
-    items.append(item.__dict__)
-  return jsonify(items)
+    items = [item_to_dict(item) for item in Item.query.all()]
+    return jsonify(items)
 
 @app.route('/items', methods=['POST'])
 def create_item():
-  body = request.get_json()
-  db.session.add(Item(body['title'], body['content']))
-  db.session.commit()
-  return "item created"
+    data = request.get_json()
+    new_item = Item(data['title'], data['content'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify(message='Item created')
 
-@app.route('/items/<id>', methods=['PUT'])
+@app.route('/items/<int:id>', methods=['PUT'])
 def update_item(id):
-  body = request.get_json()
-  db.session.query(Item).filter_by(id=id).update(
-    dict(title=body['title'], content=body['content']))
-  db.session.commit()
-  return "item updated"
+    item = Item.query.get(id)
+    if item is None:
+        return jsonify(message='Item not found'), 404
+    
+    data = request.get_json()
+    item.title = data['title']
+    item.content = data['content']
+    db.session.commit()
+    return jsonify(message='Item updated')
 
-@app.route('/items/<id>', methods=['DELETE'])
+@app.route('/items/<int:id>', methods=['DELETE'])
 def delete_item(id):
-  db.session.query(Item).filter_by(id=id).delete()
-  db.session.commit()
-  return "item deleted"
+    item = Item.query.get(id)
+    if item is None:
+        return jsonify(message='Item not found'), 404
+    db.session.delete(item)
+    db.session.commit()
+    return jsonify(message='Item deleted')
 
+# Additional Endpoints
 @app.route('/')
 def helloroot():
     return jsonify(message='Hello from AWS Lambda using Python Flask in Docker!')
 
 @app.route('/health')
-def hello():
-    logger.info('## API for health check initiated ')
-    return jsonify(message='health okay')
+def health():
+    logger.info('## API health check initiated')
+    return jsonify(message='Health okay')
 
 @app.route('/echo', methods=['POST'])
 def echo():
-    
     data = request.get_json()
-    logger.info('## echo in logs also %s ',data )
+    logger.info('## Echo data: %s', data)
     return jsonify(data)
 
-
+# Lambda Handler
 def lambda_handler(event, context):
-    logger.info('## API event : %s', event)
-    responseApi = awsgi.response(app, event, context)
-    logger.info('## API response : %s', responseApi)
-    return responseApi
+    logger.info('## API event: %s', event)
+    response = awsgi.response(app, event, context)
+    logger.info('## API response: %s', response)
+    return response
